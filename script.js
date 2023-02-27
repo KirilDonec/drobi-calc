c7dbc34b52702721e4c0ae84ba111856dd7c92ff
KirilDonec <126416531+KirilDonec@users.noreply.github.com>
2023-02-27 20:09:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,77 @@
-function gcd(a, b) {
-  if (b == 0) {
-    return a;
+// Функция для поиска НОД
+function findGcd(x, y) {
+  while (y) {
+    var t = y;
+    y = x % y;
+    x = t;
   }
-  return gcd(b, a % b);
+  return x;
 }
 
-function simplify(numerator, denominator) {
-  const divisor = gcd(numerator, denominator);
-  return [numerator / divisor, denominator / divisor];
+// Функция для сокращения дроби
+function reduceFraction(numerator, denominator) {
+  var gcd = findGcd(numerator, denominator);
+  numerator /= gcd;
+  denominator /= gcd;
+  return [numerator, denominator];
 }
 
-function addFractions(a, b) {
-  const numerator = a[0] * b[1] + b[0] * a[1];
-  const denominator = a[1] * b[1];
-  return simplify(numerator, denominator);
+// Функция для сложения дробей
+function addFractions() {
+  var numerator1 = parseInt(document.getElementById("numerator1").value);
+  var denominator1 = parseInt(document.getElementById("denominator1").value);
+  var numerator2 = parseInt(document.getElementById("numerator2").value);
+  var denominator2 = parseInt(document.getElementById("denominator2").value);
+
+  var newNumerator = numerator1 * denominator2 + numerator2 * denominator1;
+  var newDenominator = denominator1 * denominator2;
+
+  var result = reduceFraction(newNumerator, newDenominator);
+
+  document.getElementById("result").value = result[0] + "/" + result[1];
 }
 
-function subtractFractions(a, b) {
-  const numerator = a[0] * b[1] - b[0] * a[1];
-  const denominator = a[1] * b[1];
-  return simplify(numerator, denominator);
+// Функция для вычитания дробей
+function subtractFractions() {
+  var numerator1 = parseInt(document.getElementById("numerator1").value);
+  var denominator1 = parseInt(document.getElementById("denominator1").value);
+  var numerator2 = parseInt(document.getElementById("numerator2").value);
+  var denominator2 = parseInt(document.getElementById("denominator2").value);
+
+  var newNumerator = numerator1 * denominator2 - numerator2 * denominator1;
+  var newDenominator = denominator1 * denominator2;
+
+  var result = reduceFraction(newNumerator, newDenominator);
+
+  document.getElementById("result").value = result[0] + "/" + result[1];
 }
 
-function multiplyFractions(a, b) {
-  const numerator = a[0] * b[0];
-  const denominator = a[1] * b[1];
-  return simplify(numerator, denominator);
+// Функция для умножения дробей
+function multiplyFractions() {
+  var numerator1 = parseInt(document.getElementById("numerator1").value);
+  var denominator1 = parseInt(document.getElementById("denominator1").value);
+  var numerator2 = parseInt(document.getElementById("numerator2").value);
+  var denominator2 = parseInt(document.getElementById("denominator2").value);
+
+  var newNumerator = numerator1 * numerator2;
+  var newDenominator = denominator1 * denominator2;
+
+  var result = reduceFraction(newNumerator, newDenominator);
+
+  document.getElementById("result").value = result[0] + "/" + result[1];
 }
 
-function divideFractions(a, b) {
-  const numerator = a[0] * b[1];
-  const denominator = a[1] * b[0];
-  return simplify(numerator, denominator);
-}
+// Функция для деления дробей
+function divideFractions() {
+  var numerator1 = parseInt(document.getElementById("numerator1").value);
+  var denominator1 = parseInt(document.getElementById("denominator1").value);
+  var numerator2 = parseInt(document.getElementById("numerator2").value);
+  var denominator2 = parseInt(document.getElementById("denominator2").value);
 
-// пример использования функций для сложения дробей
-const a = [1, 2];
-const b = [1, 3];
-const sum = addFractions(a, b);
-console.log(sum); // [5, 6]
+  var newNumerator = numerator1 * denominator2;
+  var newDenominator = numerator2 * denominator1;
+
+  var result = reduceFraction(newNumerator, newDenominator);
+
+  document.getElementById("result").value = result[0] + "/" + result[1];
+}
